d82145dc7a938acd879ff037c0ce8163bfff0152
Duzgun Dogan <duzgundogan0@gmail.com>
2026-03-23 15:19:52 +0300

    las

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -4,11 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.URL;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class DriverFactory {
 
@@ -24,13 +20,29 @@ public class DriverFactory {
 	private static WebDriver createDriver() {
 		ChromeOptions options = new ChromeOptions();
 
+		// Sunucu ortamında (Linux/Docker) çalışması için zorunlu ayarlar
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
 		options.addArguments("--window-size=1920,1080");
 
-		// ❗ HEADLESS KALDIR (QAMaster kendi yönetir)
-		// options.addArguments("--headless=new");
+		// SUNUCUDA ÇALIŞIYORSAN HEADLESS ŞARTTIR
+		options.addArguments("--headless=new");
 
-		return new ChromeDriver(options);
+		try {
+			// QAMaster veya Sistem Değişkenlerinden gelen Hub URL'ini kontrol et
+			String hubUrl = System.getProperty("DQAMASTER_HUB_URL");
+
+			if (hubUrl != null && !hubUrl.isEmpty()) {
+				System.out.println("Connecting to Remote Driver at: " + hubUrl);
+				return new RemoteWebDriver(new URL(hubUrl), options);
+			} else {
+				// Eğer hub yoksa yerel çalıştır (Local debug için)
+				return new ChromeDriver(options);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Driver başlatılamadı!");
+		}
 	}
 }
