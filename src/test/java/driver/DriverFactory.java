5271acaf9dbdfe3e8a29cca113a4fd2d59821060
Duzgun Dogan <duzgundogan0@gmail.com>
2026-03-23 09:37:18 +0300

    remote driver added

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -2,39 +2,42 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.URL;
 import java.util.Collections;
 
 public class DriverFactory {
 
-    // Get a new WebDriver Instance.
-    // There are various implementations for this depending on browser. The required browser can be set as an environment variable.
-    // Refer http://getgauge.io/documentation/user/current/managing_environments/README.html
-    public static WebDriver getDriver() {
+	public static WebDriver getDriver() {
 
+		ChromeOptions options = new ChromeOptions();
 
-
-	            ChromeOptions options = new ChromeOptions();
-	            if ("Y".equalsIgnoreCase(System.getenv("HEADLESS"))) {
-
-	                options.addArguments("--headless");
-	                options.addArguments("--disable-gpu");
-	            }
-		options.addArguments("--incognito");
 		options.addArguments("--headless=new");
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
-		options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-		options.setExperimentalOption("useAutomationExtension", false);
+		options.addArguments("--incognito");
 		options.addArguments("--disable-blink-features=AutomationControlled");
+
 		options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
 		options.setExperimentalOption("useAutomationExtension", false);
 
-		options.addArguments("--disable-blink-features=AutomationControlled");
-	            return new ChromeDriver(options);
-        }
-    }
+		try {
+			String browserType = System.getProperty("browserType");
+			String hubUrl = System.getProperty("hubUrl");
 
+			// 🔴 CRITICAL FIX: Remote destek
+			if ("Remote".equalsIgnoreCase(browserType)) {
+				return new RemoteWebDriver(new URL(hubUrl), options);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// default local
+		return new ChromeDriver(options);
+	}
+}
